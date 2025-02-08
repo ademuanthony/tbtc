9832a3a9912fd25b95a25af481a7c5d59d7dd71b
Ademu Anthony <ademuanthony@gmail.com>
2025-02-08 11:24:28 +0100

    clean up

--- uniswap-v3/libs/readline.ts
+++
@@ -1,9 +0,0 @@
-import * as readline from 'readline';
-
-export const getStdInput = async (question: string, rl: readline.Interface): Promise<string> => {
-  return new Promise((resolve) => {
-    rl.question(question, (answer: string) => {
-      resolve(answer);
-    });
-  });
-}
